488976d006f997bbb38363d151a26ca6d190acb4
Lacerto <christopher.s.j.campbell@googlemail.com>
2016-07-29 11:41:43 +0100

    added tests for 100% coverage

--- inst/examples/sales/inst/tests/testthat/tests2/test_sales2.R
+++ inst/examples/sales/inst/tests/testthat/tests2/test_sales2.R
@@ -1,8 +1,10 @@
-# Date of last change: 2016-01-08
+# Date of last change: 2016-07-29
 # Last changed by: ccampbell
-# 
 # Original author: ccampbell
+# 
 # Copyright Mango Solutions, Chippenham, UK 2016
+# This document may be copied whole or in part for any use, 
+# provided that this copyright notice is retained.
 ###############################################################################
 
 context("test expected")
@@ -22,19 +24,15 @@ test_that("hit symbols in expected", {
             .Names = c("Date", "Daily.Total"), 
             row.names = "1", 
             class = "data.frame"))
-
-})
-
-
-test_that("validation", {
     
-    x <- data.frame(Date = as.Date(x = 1:3, origin = "2015-12-18"),
-        Daily.Total = 1:3,
-        Outlet = "CHIPPENHAM")
+    expect_error(object = expected(x = women, when = as.Date("2015-12-22")), 
+        regex = "x must be SalesData")
     
-    expect_error(object = expected(x = x, when = as.Date("2015-12-22")), 
-        regexp = "x must be SalesData")
-
+    expect_equal(object = class(expected(x = x)), expected = "data.frame")
+    
+    expect_error(object = expected(x = x, when = "2015-12-22"), 
+        regex = "when must be Date")
+    
 })
 
 
@@ -52,6 +50,8 @@ test_that("hit symbols in as.data.frame and unique", {
     
     expect_equal(object = unique(x = x), 
         expected = 1:3)
+    
+    expect_message(object = print(x), regex = "SalesData for")
 
 })
 
